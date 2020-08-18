4307a4bbaa76ec0d30ae3f8e8c31b266569b5e2c
Rob Chapman <autosexual@gmail.com>
2020-08-18 14:12:39 +0900

    added dependent destroy for project steps

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :user
-  has_many :project_steps
+  has_many :project_steps, dependent: :destroy
   has_many :user_projects
   has_many :users, through: :user_projects
   validates :title, presence: true
