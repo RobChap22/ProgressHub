ab8f5a6f791e2a12068a3683a4265cd199941186
Rob Chapman <autosexual@gmail.com>
2020-08-26 16:12:31 +0900

    pie chart up and running

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,12 @@ class UsersController < ApplicationController
     @finished_projects = policy_scope(UserProject).where(completed: true)
     skip_authorization
     @quote = Quote.all.sample
+    @tag_hash = finished_project_tags
+  end
+
+  def finished_project_tags
+    tag_hash = Hash.new(0)
+    Project.joins(:user_projects).where(user_projects: {completed: true}).each { |proj| proj.tag_list.each { |tag| tag_hash[tag] += 1}}
+    tag_hash
   end
 end
